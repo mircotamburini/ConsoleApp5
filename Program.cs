2c363599d020907d2f689f7d88cb7751684c9bc3
mirco tamburini <mircotamburini@libero.it>
2020-04-20 22:59:42 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp5
     {   
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worldvv!");
+            Console.WriteLine("Hello Worldv!");
         }
     }
 }
